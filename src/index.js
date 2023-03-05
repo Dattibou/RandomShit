92a93e5134cf1c8a4af1ea8897ea6cb004461bc3
Dattibou <b.datduc@yahoo.de>
2023-03-05 23:46:17 +0100

    Let's get shit started !

---
+++ src/index.js
@@ -0,0 +1,18 @@
+/*import * as dvb from "dvbjs";
+
+const stopID = "33000037";
+
+dvb.monitor(stopID).then((data) => {
+    console.dir(data);
+  });
+  */
+
+import * as dvb from "dvbjs"; // OR const dvb = require("dvbjs");
+
+const stopID = "33000037"; // Postplatz
+const timeOffset = 5;
+const numResults = 2;
+
+dvb.monitor(stopID, timeOffset, numResults).then((data) => {
+    console.dir(data, { depth: 7, maxArrayLength: 2 });
+});
